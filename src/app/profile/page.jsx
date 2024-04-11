6d515f1b4a54737f13a8b0dfc19d6a457c206a8b
ManonEche <manon.eche@laposte.net>
2024-04-11 15:02:01 +0200

    Formulaire - Partie 2

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,9 +1,15 @@
+"use client";
+
 import Button from "@/components/Button/Button";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
-import { ChevronDown, Info } from 'lucide-react';
+import { ChevronDown, Info, X } from 'lucide-react';
+import { useState } from "react";
 
 export default function Profile() {
+  // State
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <>
       <div className="min-h-screen w-screen">
@@ -16,32 +22,37 @@ export default function Profile() {
               <form className="flex flex-col items-center gap-4 text-xl pb-10">
 
                 <div className="flex flex-col items-center w-full gap-2">
-                  <label for="lastname" className="text-start w-3/4">Nom</label>
-                  <input type="text" name="lastname" className="w-3/4 rounded-2xl px-5 py-3 text-xl disabled:opacity-75 disabled:cursor-not-allowed" value="Eche" disabled />
+                  <label htmlFor="lastname" className="text-start w-3/4">Nom</label>
+                  <input type="text" name="lastname" className="w-3/4 rounded-2xl px-5 py-3 text-xl disabled:bg-white disabled:opacity-60 disabled:cursor-not-allowed" value="Eche" disabled />
                 </div>
 
                 <div className="flex flex-col items-center w-full gap-2">
-                  <label for="firstname" className="text-start w-3/4">Prénom</label>
-                  <input type="text" name="firstname" className="w-3/4 rounded-2xl px-5 py-3 text-xl disabled:opacity-75 disabled:cursor-not-allowed" value="Manon" disabled />
+                  <label htmlFor="firstname" className="text-start w-3/4">Prénom</label>
+                  <input type="text" name="firstname" className="w-3/4 rounded-2xl px-5 py-3 text-xl disabled:bg-white disabled:opacity-60 disabled:cursor-not-allowed" value="Manon" disabled />
                 </div>
 
                 <div className="flex flex-col items-center w-full gap-2">
-                  <label for="email" className="text-start w-3/4">Email</label>
-                  <input type="email" name="email" className="w-3/4 rounded-2xl px-5 py-3 text-xl disabled:opacity-75 disabled:cursor-not-allowed" value="manon@manon.com" disabled />
+                  <label htmlFor="email" className="text-start w-3/4">Email</label>
+                  <input type="email" name="email" className="w-3/4 rounded-2xl px-5 py-3 text-xl disabled:bg-white disabled:opacity-60 disabled:cursor-not-allowed" value="manon@manon.com" disabled />
                 </div>
 
                 <div className="flex flex-col items-center w-full gap-2">
-                  <label for="tel" className="text-start w-3/4">Numéro de téléphone</label>
+                  <label htmlFor="tel" className="text-start w-3/4">Numéro de téléphone</label>
                   <input type="tel" name="tel" className="w-3/4 rounded-2xl px-5 py-3 text-xl" required />
                 </div>
 
                 <div className="flex flex-col items-center w-full gap-2">
-                  <label for="date" className="text-start w-3/4">Date de naissance</label>
+                  <label htmlFor="date" className="text-start w-3/4">Date de naissance</label>
                   <input type="date" name="date" className="w-3/4 rounded-2xl px-5 py-3 text-xl" required />
                 </div>
 
                 <div className="relative flex flex-col items-center w-3/4 gap-2">
-                  <label for="skin" className="text-start w-full flex gap-2 items-center">Type de peau <Info className="w-5"/></label>
+                  <label htmlFor="skin" className="text-start w-full flex gap-2 items-center">
+                    Type de peau
+                    <button onClick={() => setOpenModal(true)}>
+                      <Info className="w-5" />
+                    </button>
+                  </label>
                   <select id="skin" name="skin" className="appearance-none w-full rounded-2xl px-5 py-3 text-lg" required>
                     <option value="">Sélectionner :</option>
                     <option value="dry">Sèche</option>
@@ -50,7 +61,25 @@ export default function Profile() {
                     <option value="oily">Grasse</option>
                     <option value="nothing">Ne sais pas</option>
                   </select>
-                    <ChevronDown className="absolute pointer-events-none bottom-3 right-3" />
+                  <ChevronDown className="absolute pointer-events-none bottom-3 right-3" />
+
+                  {/* Modal */}
+                  {openModal && (
+                    <div className="fixed inset-0 bg-calme-dark bg-opacity-50 flex justify-center items-center z-50">
+                      <div className="bg-calme-light py-5 px-8 rounded-2xl w-1/2">
+                        <div className="absolute w-1/2 right-8">
+                          <button onClick={() => setOpenModal(false)}><X /></button>
+                        </div>
+                        <div className="text-start whitespace-pre-line">
+                          <p><b>Peau sèche :</b> manque de sébum, tiraillements et inconforts.</p>
+                          <p><b>Peau normale :</b> aucun excès ou manque de sébum.</p>
+                          <p><b>Peau mixte :</b> excès de sébum sur la zone "T"
+                            (menton, nez et front). Les autres zones sont normales à sèches.</p>
+                          <p><b>Peau grasse :</b> excès de sébum présent sur l'ensemble du visage.</p>
+                        </div>
+                      </div>
+                    </div>
+                  )}
                 </div>
 
               </form>
