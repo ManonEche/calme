d12f3885b674bc951762493468d5d7a3cd9d61ea
ManonEche <manon.eche@laposte.net>
2024-05-10 13:46:20 +0200

    Correction footer

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -150,7 +150,7 @@ export default function Login() {
                   </Link>
                 </div>
               </div>
-              <div className="absolute left-0 bottom-0">
+              <div className="pt-3 sm:absolute sm:left-0 sm:bottom-0">
                 <Footer />
               </div>
             </div>
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -134,7 +134,7 @@ export default function Signup() {
                   </Link>
                 </div>
               </div>
-              <div className="absolute left-0 bottom-0">
+              <div className="pt-3 sm:absolute sm:left-0 sm:bottom-0">
                 <Footer />
               </div>
             </div>
